5cd268497855cfee2effded777bc76a2f5766251
Xiaobin Li <l_xb@foxmail.com>
2015-11-14 16:00:31 +0800

    fix xcode7 warnings

--- Classes/Explorer Toolbar/FLEXExplorerViewController.m
+++ Classes/Explorer Toolbar/FLEXExplorerViewController.m
@@ -194,7 +194,7 @@ typedef NS_ENUM(NSUInteger, FLEXExplorerMode) {
 
 #pragma mark - Rotation
 
-- (NSUInteger)supportedInterfaceOrientations
+- (UIInterfaceOrientationMask)supportedInterfaceOrientations
 {
     UIViewController *viewControllerToAsk = [self viewControllerForStatusBarAndOrientationProperties];
     NSUInteger supportedOrientations = [FLEXUtility infoPlistSupportedInterfaceOrientationsMask];
